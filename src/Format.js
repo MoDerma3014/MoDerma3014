39cc4eb6b1e97d1fb59bf0c245439bca0baf3549
MoDerma3014 <113134469+MoDerma3014@users.noreply.github.com>
2023-03-05 13:32:26 -0700

    done top

--- src/Format.js
+++ src/Format.js
@@ -3,11 +3,14 @@ import React from "react";
 export default function Format() {
     return (
     <div>
-    <button id="sidebutton"></button>       
+    
     <div class="top-bar">
-        
-        <div class="top-bar-title">Lotion</div>
-        <div class="top-bar-sub">Like Notion, but worse</div>
+        <button id="sidebutton">Menu</button>
+        <div class="title-container">
+            <div class="top-bar-title">Lotion</div>
+            <div class="top-bar-title">Like Notion, but worse</div>
+        </div>
+        <div></div>
     </div>
     
     </div>       
